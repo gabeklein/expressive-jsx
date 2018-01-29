a18b585bbd31091f74212c15e715b574263e1709
Gabe Klein <gabe@termtm.com>
2018-01-29 16:56:10 -0500

    inferred components now will implicitly import react

--- packages/babel-plugin-infer-extends-component/src/index.js
+++ packages/babel-plugin-infer-extends-component/src/index.js
@@ -1,6 +1,57 @@
 export default function babel_plugin_inferred_react_component(){
     return {
         visitor: {
+            Program: {
+                enter(path, state){
+
+                    const {scope}  = path;
+                    const {file} = this;
+
+                    const _component = scope.generateUidIdentifier("_component");
+
+                    Object.defineProperties(state, {
+                        ref_component: {
+                            get: componentWasInfered,
+                            configurable: true
+                        }
+                    })
+
+                    function componentWasInfered() {
+                        state.extention_used = true;
+                        Object.defineProperty(state, "ref_component", { value: _component })
+                        return _component
+                    }
+                },
+                exit(path, state){
+                    const { file } = this;
+                    const {scope}  = path;
+
+                    if(state.extention_used){
+                        let _react = file.expressive_ref_react;
+
+                        if(!_react){
+                            _react 
+                                = file.expressive_ref_react 
+                                = scope.generateUidIdentifier("react");
+                            scope.push({
+                                kind: "const",
+                                id: _react,
+                                init: t.callExpression(
+                                    t.identifier("require"), [
+                                        t.stringLiteral("react")
+                                    ]
+                                )
+                            })
+                        }
+
+                        path.scope.push({ 
+                            kind: "const", 
+                            id: state.ref_component,
+                            init: t.memberExpression(_react, t.identifier("Component"))
+                        })
+                    }
+                }
+            },
             Class: {
                 enter: classMayBeComponent
             }
@@ -10,13 +61,7 @@ export default function babel_plugin_inferred_react_component(){
 
 const t = require("babel-types");
 
-const componentAST = 
-    t.memberExpression(
-        t.identifier("React"),
-        t.identifier("Component")
-    )
-
-function classMayBeComponent(path, _state){
+function classMayBeComponent(path, state){
     let classIsConsideredComponent = false;
 
     for(const property of path.get("body.body")){
@@ -36,6 +81,6 @@ function classMayBeComponent(path, _state){
         if(alreadyExtends.isNullLiteral())
             path.node.superClass = null;
         else if(alreadyExtends.node == null)
-            path.node.superClass = componentAST
+            path.node.superClass = state.ref_component
     }
 }
