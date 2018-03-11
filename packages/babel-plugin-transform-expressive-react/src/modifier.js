d2ef48c12bd32cb5c721e19d149991adccf9e496
Gabe Klein <gabe@termtm.com>
2018-03-10 19:49:08 -0500

    renamed SpecialModifier

--- packages/babel-plugin-transform-expressive-react/src/modifier.js
+++ packages/babel-plugin-transform-expressive-react/src/modifier.js
@@ -1,7 +1,11 @@
 import { ComponentModifier } from "./attributes";
+const { Prop, Style, Statement, ChildNonComponent } = require("./item");
 
-export class SpecialModifier extends ComponentModifier {
-    static applyTo(parent, path){
+export class GeneralModifier {
+    static handle(parent, path){
+        const { type } = path;
+        if(type == "Labeled"){
 
+        }
     }
 }
