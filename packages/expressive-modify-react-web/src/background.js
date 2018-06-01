e664e31b76eeed322151573fc3b0d2b548fe4385
Gabe Klein <gabe@termtm.com>
2018-05-31 23:31:43 -0400

    added HSL processing

--- packages/expressive-modify-react-web/src/background.js
+++ packages/expressive-modify-react-web/src/background.js
@@ -11,6 +11,19 @@ export function rgba(r,g,b,a = 1){
     }
 }
 
+export { hsla as hsl }
+
+export function hsla(h, s, l, a = 1){
+    for(const x of [h,s,l])
+        if(typeof x != "number") 
+            throw new Error("malformed arguments in hsl statement")
+
+    const hsl = [h,s+"%",l+"%"].join(",");
+    return {
+        value: a == 1 ? `hsl(${hsl})` : `hsla(${hsl},${a})`
+    }
+}
+
 export function backgroundImage(a){
     if(typeof a == "object" && !a.named)
         return { style: {
