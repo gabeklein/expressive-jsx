24fefd1ffb888b882cfdee94b8981d32b72e5a1e
Gabe Klein <gabe@termtm.com>
2018-07-05 04:29:37 -0400

    fixed also, and modifier binding

--- packages/babel-plugin-transform-expressive-react/src/keywords.js
+++ packages/babel-plugin-transform-expressive-react/src/keywords.js
@@ -1,3 +1,5 @@
+import { ComponentModifier } from "./modifier"
+
 export function is(){
     const out = { 
         attrs: {}, props: {}, style: {}
@@ -26,13 +28,13 @@ export function and(){
 
     for(const alias of arguments)
         if(typeof alias == "string")
-            if(temp = target.context["$" + alias])
+            if(temp = target.context["__" + alias])
                 if(temp == target)
                     throw new Error("Bad argument, a component really shouldn't alias itself")
                 else if(temp.inherits)
                     clone.inherits = temp.inherits,
                     temp.inherits = clone;
                 else temp.inherits = clone;
-            else Object.getPrototypeOf(target.context)["$" + alias] = clone;
+            else Object.getPrototypeOf(target.context)["__" + alias] = clone;
         else throw new Error("Bad argument,\"on\" modifiers expect identifiers or strings.")
 }
