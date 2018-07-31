baf2d3d5c7bb85cfa0c340c37d7e169a4ea183a2
Gabe Klein <gabe@termtm.com>
2018-07-31 18:51:32 -0400

    font upgrade

--- packages/expressive-modify-react-web/src/text.js
+++ packages/expressive-modify-react-web/src/text.js
@@ -1,8 +1,33 @@
 export function font(a, b = null){
+    if(typeof a == "string" && !/^[0-9]+/.test(a))
+        return {
+            attrs: {
+                fontFamily: a
+            }
+        }
     return {
         attrs: {
             fontSize: a,
             fontWeight: b
+        }
+    }
+}
+
+export { fontFamily as family }
+
+export function fontFamily(){
+
+    const fam = this.arguments
+        .map((font) => {
+            if(~font.indexOf(" "))
+                return `"${font}"`
+            else return font
+        })
+        .join(", ");
+
+    return {
+        style: {
+            fontFamily: fam
         }
     }
 }
