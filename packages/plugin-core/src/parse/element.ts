133ccf683d6d1bb88093eb1ca16b67fc3b08ad2e
Gabe <gabe@termtm.com>
2019-04-09 19:47:27 -0400

    temporary fix to recursive modifiers

--- packages/plugin-core/src/parse/element.ts
+++ packages/plugin-core/src/parse/element.ts
@@ -108,6 +108,10 @@ export function ApplyNameImplications(
 
     do {
         modify.apply(target);
+        if(modify === modify.inherits){
+            // debugger
+            break
+        }
         modify = modify.inherits;
     }
     while(modify);
