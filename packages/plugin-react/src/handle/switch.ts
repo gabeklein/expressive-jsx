6ffeb5fd17b9115047ea1d1bfa85e375f6bbcc56
Gabe <gabe@termtm.com>
2019-04-11 15:16:23 -0400

    new switch-expression algorithm

--- packages/plugin-react/src/handle/switch.ts
+++ packages/plugin-react/src/handle/switch.ts
@@ -1,7 +1,30 @@
 import t, { Expression } from '@babel/types';
-import { ComponentConsequent, ComponentIf } from '@expressive/babel-plugin-core';
+import { ComponentIf } from '@expressive/babel-plugin-core';
 import { ElementReact, GenerateReact } from 'internal';
 import { StackFrameExt } from 'types';
+
+const opt = t.conditionalExpression;
+const not = (a: Expression) => t.unaryExpression("!", a);
+const and = (a: Expression, b: Expression) => t.logicalExpression("&&", a, b);
+
+//TODO: figure out if falsey values interfere before allowing them through
+// const anti = (a: Expression) => t.isUnaryExpression(a, { operator: "!" }) ? a.argument : not(a);
+const anti = not; 
+
+function fork(
+    test?: Expression, 
+    product?: Expression, 
+    rest?: Expression
+): Expression | undefined {
+    if(rest && test)
+        return product
+            ? opt(test, product, rest)
+            : and(anti(test), rest)
+    if(product)
+        return test
+            ? and(test, product)
+            : product
+}
 
 export class ElementSwitch {
 
@@ -11,47 +34,27 @@ export class ElementSwitch {
     };
 
     toExpression(){
-        const { forks } = this.source;
-        if(forks.length > 1)
-            return forks.reduceRight(
-                this.inlineReduction.bind(this), 
-                t.booleanLiteral(false)
-            );
-        else {
-            let { test, product } = this.extract(forks[0]);
-            
-            let check: Expression = test!;
-
-            if(check.type == "LogicalExpression")
-                check = check.right;
-                
-            if(check.type != "BooleanLiteral" 
-            && check.type != "BinaryExpression")
-                check = t.unaryExpression("!", t.unaryExpression("!", check))
-
-            return t.logicalExpression("&&", check, product);
-        }
-    }
-
-    inlineReduction(alternate: Expression, current: ComponentConsequent){
-        const { test, product } = this.extract(current);
-        return test 
-            ? t.conditionalExpression(test, product, alternate)
-            : product
-    }
-
-    extract(item: ComponentConsequent): { test?: Expression, product: Expression } {
-        const { test } = item;
-
-        const content = new ElementReact(item);
-
         const Generator = this.context.Generator as GenerateReact;
+        const conditions = this.source.forks;
+        let sum: Expression | undefined;
+        let i = conditions.length;
 
-        const product = Generator.container(content)
-        
-        return {
-            test: test && test.node,
-            product
-        };
+        while(i > 0){
+            let product;
+            const cond = conditions[--i]
+            const test = cond.test && cond.test.node;
+
+            if(cond.children.length)
+                product = Generator.container(
+                    new ElementReact(cond)
+                )
+
+            if(t.isBooleanLiteral(product, { value: false }))
+                product = undefined;
+
+            sum = fork(test, product, sum)
+        }
+
+        return sum || t.booleanLiteral(false)
     }
 }
