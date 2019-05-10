055a8618da1e0833d5faca9c0698ed6312d43d71
Gabe <gabe@termtm.com>
2019-05-09 21:37:32 -0400

    better bg modifier now parses rgba, hsla

--- packages/modify-react/src/background.js
+++ packages/modify-react/src/background.js
@@ -1,4 +1,5 @@
 const { URL_IMAGES } = process.env;
+const { rgba, hsla } = require("./helpers.js")
 
 export function image(a){
     const CDN = process.env.CDN || "";
@@ -13,20 +14,40 @@ export function image(a){
 }
 
 export function bg(a){
-    let value;
+    let output;
+
     if(Array.isArray(a)){
-        return {
-            style: {
-                backgroundImage: `url(${URL_IMAGES + a})`
+        const [ head, ...tail ] = a;
+        switch(head){
+            case "url":
+                output = {
+                    backgroundImage: `url(${URL_IMAGES + tail[1]})`
+                };
+                break;
+
+            case "rgb":
+            case "rgba": {
+                const { value } = rgba(...tail);
+                output = { backgroundColor: value };
+                break;
+            }
+
+            case "hsl":
+            case "hsla": {
+                const { value } = hsla(...tail);
+                output = { backgroundColor: value };
+                break;
             }
         }
     }
     else {
-        return {
-            style: {
-                backgroundColor: a
-            }
+        output = {
+            background: a
         }
+    }
+
+    return {
+        style: output
     }
 }
 
