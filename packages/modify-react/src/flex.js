2a534117a7bb2932e4f78179a322e9991c703193
Gabe <gabe@termtm.com>
2019-05-31 04:57:10 -0400

    better flex handling

--- packages/modify-react/src/flex.js
+++ packages/modify-react/src/flex.js
@@ -1,25 +1,59 @@
-const DIRECTION = {
+// const DIRECTION = {
+//     right: "row",
+//     left: "row-reverse",
+//     up: "column-reverse",
+//     down: "column"
+// }
+
+// export function flexAlign(mode, direction = "right"){
+
+//     const style = {
+//         display: "flex"
+//     }
+
+//     if(mode == "center")
+//         style.justifyContent = 
+//         style.alignItems = 
+//         "center"
+//     else
+//         style.justifyContent = mode;
+
+//     if(direction = DIRECTION[direction] || direction)
+//         style.flexDirection = direction;
+
+//     return { style };
+// }
+
+const FlexDirections = {
     right: "row",
     left: "row-reverse",
     up: "column-reverse",
-    down: "column"
+    down: "column",
+    row: null,
+    column: null,
+    "row-reverse": null,
+    "column-reverse": null
 }
 
-export function flexAlign(mode, direction = "right"){
+export function flexAlign(){
 
     const style = {
         display: "flex"
     }
 
-    if(mode == "center")
-        style.justifyContent = 
-        style.alignItems = 
-        "center"
-    else
-        style.justifyContent = mode;
-
-    if(direction = DIRECTION[direction] || direction)
-        style.flexDirection = direction;
+    for(const arg of this.arguments){
+        if(arg in FlexDirections)
+            style.flexDirection = 
+                FlexDirections[arg] || arg;
+        else
+        if(arg == "center")
+            style.justifyContent = 
+            style.alignItems = 
+            "center"
+        else
+            style.justifyContent = arg;
+    }
 
     return { style };
 }
+
