02c01f7986cb0c6d239fb7d7fc7f143bd45892d8
Gabe <gabe@termtm.com>
2019-06-18 01:39:56 -0400

    make plugin-react usable again

--- packages/preset-react/index.js
+++ packages/preset-react/index.js
@@ -1,25 +1,16 @@
-const PluginEnhance = require("babel-preset-expressive-enhancements").default;
-const PluginReactClass = require("@expressive/babel-plugin-react-class").default;
 const PluginReact = require("@expressive/babel-plugin-react")
 const WebStyles = require("@expressive/react-modifiers");
 
-module.exports = function(options){
-    return {
-        presets: [
-            PluginEnhance
-        ],
-        plugins: [
-            [PluginReactClass, {
-                activeOnMethodDo: true
-            }],
-            [PluginReact, {
-                reactEnv: "web",
-                output: "es6",
-                styleMode: "compile",
-                modifiers: [
-                    WebStyles
-                ]
-            }],
-        ]
-    }
+module.exports = (options = {}) => {
+  let modifiers = [ WebStyles ].concat(options.modifiers || []);
+
+  return {
+    plugins: [
+      [PluginReact, {
+        reactEnv: "web",
+        output: "js",
+        modifiers
+      }]
+    ]
+  }
 }
