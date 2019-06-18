548d4d06a1eff3637d20410837a1b56e55b5b264
Gabe <gabe@termtm.com>
2019-06-18 02:48:54 -0400

    fix arguments in babel-preset

--- packages/preset-react/index.js
+++ packages/preset-react/index.js
@@ -1,7 +1,7 @@
 const PluginReact = require("@expressive/babel-plugin-react")
 const WebStyles = require("@expressive/react-modifiers");
 
-module.exports = (options = {}) => {
+module.exports = (compiler, options = {}) => {
   let modifiers = [ WebStyles ].concat(options.modifiers || []);
 
   return {
