0f45fe1edc0063dd918c0684642ddfc313915596
Gabe <gabe@termtm.com>
2019-06-23 02:43:50 -0400

    forward opts to plugin

--- packages/preset-react/index.js
+++ packages/preset-react/index.js
@@ -2,14 +2,16 @@ const PluginReact = require("@expressive/babel-plugin-react")
 const WebStyles = require("@expressive/react-modifiers");
 
 module.exports = (compiler, options = {}) => {
-  let modifiers = [ WebStyles ].concat(options.modifiers || []);
+  let { modifiers, ...opts } = options;
+  modifiers = [ WebStyles ].concat(modifiers || []);
 
   return {
     plugins: [
       [PluginReact, {
         reactEnv: "web",
         output: "js",
-        modifiers
+        modifiers,
+        ...opts
       }]
     ]
   }
