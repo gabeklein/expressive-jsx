4eef661b8552f055747d832ef9a35bce33e4fc6f
Gabe <gabe@termtm.com>
2019-06-26 05:12:38 -0400

    better naming for forStatement elements

--- packages/plugin-core/src/handle/iterate.ts
+++ packages/plugin-core/src/handle/iterate.ts
@@ -1,5 +1,5 @@
 import { NodePath as Path } from '@babel/traverse';
-import { AssignmentExpression, expressionStatement, For } from '@babel/types';
+import { AssignmentExpression, expressionStatement, For, isVariableDeclaration, isIdentifier, ForXStatement, isForXStatement } from '@babel/types';
 import { StackFrame } from 'parse';
 
 import { ComponentContainer } from './';
@@ -15,6 +15,7 @@ export class ComponentFor extends ComponentContainer {
         super(context);
 
         this.node = path.node;
+        this.name = this.generateName();
 
         const body = path.get("body");
         const doBlock = this.handleContentBody(body);
@@ -23,6 +24,32 @@ export class ComponentFor extends ComponentContainer {
             body.replaceWith(
                 expressionStatement(doBlock)
             );
+    }
+
+    private generateName(){
+        const { node } = this;
+        if(isForXStatement(node)){
+            let { left, right } = this.node as ForXStatement;
+            const name = [];
+    
+            if(isVariableDeclaration(left))
+                left = left.declarations[0].id;
+    
+            if(isIdentifier(left))
+                name.push(left.name);
+    
+            name.push(node.type == "ForInStatement" ? "in" : "of");
+    
+            if(isIdentifier(right))
+                name.push(right.name);
+
+            return name.reduce(
+                (acc, w) => acc + w[0].toUpperCase() + w.slice(1),
+                "for"
+            )
+        }
+        else 
+            return "for"
     }
 
     AssignmentExpression(path: Path<AssignmentExpression>){
