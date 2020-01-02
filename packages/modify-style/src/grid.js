3b724351e00287a718429ec76f9e3d559d76bb3a
Gabe <gabe@termtm.com>
2020-01-01 23:22:21 -0500

    add css-grid modifiers

---
+++ packages/modify-style/src/grid.js
@@ -0,0 +1,88 @@
+const { from, isArray } = Array;
+
+export function gridArea(){
+  const args = from(arguments);
+  if(args.length == 2){
+    const r = {
+      style: {
+        gridRow: recombineSlash(args[0]),
+        gridColumn: recombineSlash(args[1])
+      }
+    }
+    return r;
+  }
+}
+
+export function gridRow(){
+  const args = from(arguments);
+  return {
+    style: {
+      gridRow: recombineSlash(args)
+    }
+  }
+}
+
+export function gridColumn(){
+  const args = from(arguments);
+  return {
+    style: {
+      gridColumn: recombineSlash(args)
+    }
+  }
+}
+
+export function gridRows(){
+  const args = from(arguments);
+
+  return {
+    style: {
+      display: "grid",
+      gridTemplateRows: recombineTemplate(args)
+    }
+  }
+}
+
+export function gridColumns(){
+  const args = from(arguments);
+  
+  return {
+    style: {
+      display: "grid",
+      gridTemplateColumns: recombineTemplate(args)
+    }
+  }
+}
+
+function recombineSlash(args){
+  args = [].concat(args)
+  if(args[0] == "-"){
+    let layer = args;
+    let x = "";
+    while(true){
+      x = x + " / " + layer[2];
+      if(isArray(layer[1]))
+        layer = layer[1]
+      else {
+        return layer[1] + x;
+        break;
+      }
+    }
+  }
+  else 
+    return args[0];
+}
+
+function recombineTemplate(args){
+  return args
+    .map(x => {
+      if(typeof x == "number")
+        return `${x}px`;
+      else if(typeof x == "string"){
+        if(/^\d+\.\d+$/.test(x))
+          return `${x}fr`
+        else 
+          return x;
+      }
+    })
+    .join(" ")
+}
