26b3cdf75610550fd462b91e5e5ac62aa4f7e0e5
Gabe Klein <gabe@termtm.com>
2020-07-21 03:08:59 -0400

    improve syntax, exports of ./text

--- packages/modify-style/src/text.js
+++ packages/modify-style/src/text.js
@@ -2,19 +2,16 @@ export function font(){
 
     const attrs = {};
 
-    for(const arg of this.arguments){
+    for(const arg of this.arguments)
         if(arg % 100 === 0)
             attrs.fontWeight = arg;
         else if(isNaN(arg) === false)
             attrs.fontSize = arg;
         else if(typeof arg === "string")
             attrs.fontFamily = arg
-    }
 
     return { attrs }
 }
-
-export { fontFamily as family }
 
 export function fontFamily(){
 
@@ -32,3 +29,5 @@ export function fontFamily(){
         }
     }
 }
+
+export { fontFamily as family }
