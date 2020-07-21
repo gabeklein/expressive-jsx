2232be9d4b15c5c69e423420c4dd90d8983f93a9
Gabe Klein <gabe@termtm.com>
2020-07-21 19:09:37 -0400

    hot option default to NODE_ENV=dev

--- packages/preset-react/index.js
+++ packages/preset-react/index.js
@@ -1,8 +1,14 @@
 const PluginReact = require("@expressive/babel-plugin-react");
 const styleModifiers = require("@expressive/modify-style");
 
+const { NODE_ENV } = process.env;
+
 module.exports = (compiler, options = {}) => {
-  let { modifiers = [], ...opts } = options;
+  let {
+    modifiers = [],
+    hot = NODE_ENV == "development",
+    ...opts
+  } = options;
 
   return {
     plugins: [
