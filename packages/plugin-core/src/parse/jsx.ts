05a283074a82509993153fdc1a9e163edd04cab1
Gabe Klein <gabe@termtm.com>
2020-10-13 17:43:38 -0600

    repair bad indentation in source

--- packages/plugin-core/src/parse/jsx.ts
+++ packages/plugin-core/src/parse/jsx.ts
@@ -11,7 +11,7 @@ const Error = ParseErrors({
 
 type enqueueFn = (
   element: JSXElement, parent: ElementInline
-) => ElementInline
+) => ElementInline;
 
 export function addElementFromJSX(
   node: JSXElement,
@@ -19,15 +19,15 @@ export function addElementFromJSX(
 
   const queue: [JSXElement, ElementInline][] = [];
   const push: enqueueFn = (element, parent) => {
-  const child = new ElementInline(parent.context);
-  queue.push([element, child]);
-  return child;
+    const child = new ElementInline(parent.context);
+    queue.push([element, child]);
+    return child;
   }
 
   const child = push(node, parent);
 
   for(const [element, target] of queue)
-  parseJSXElement(element, target, push);
+    parseJSXElement(element, target, push);
 
   parent.adopt(child);
 }
@@ -45,41 +45,41 @@ export function parseJSXElement(
 
   for(const attr of attributes)
     switch(attr.type){
-    case "JSXAttribute":
-      parseAttribute(attr, target);
-    break;
+      case "JSXAttribute":
+        parseAttribute(attr, target);
+      break;
 
-    case "JSXSpreadAttribute": {
-      const prop = new Prop(false, attr.argument);
-      target.add(prop);
-    } break;
+      case "JSXSpreadAttribute": {
+        const prop = new Prop(false, attr.argument);
+        target.add(prop);
+      } break;
     }
 
   for(const child of children)
     switch(child.type){
-    case "JSXElement":
-      target.adopt(
-      enqueue(child, target)
-      )
-    break;
+      case "JSXElement":
+        target.adopt(
+          enqueue(child, target)
+        )
+      break;
 
-    case "JSXText":
-      target.add(
-      stringLiteral(
-        child.value.replace(/\s+/g, " ")
-      )
-      );
-    break;
+      case "JSXText":
+        target.add(
+          stringLiteral(
+            child.value.replace(/\s+/g, " ")
+          )
+        );
+      break;
 
-    case "JSXExpressionContainer":
-      const { expression } = child;
-      target.add(
-      expression as Expression
-      );
-    break;
+      case "JSXExpressionContainer":
+        const { expression } = child;
+        target.add(
+          expression as Expression
+        );
+      break;
 
-    default:
-      throw Error.UnhandledChild(child, child.type)
+      default:
+        throw Error.UnhandledChild(child, child.type)
     }
 }
 
@@ -91,23 +91,23 @@ function parseAttribute(
   const propValue = attr.value;
 
   if(propValue === null){
-  applyNameImplications(parent, name.name);
-  return;
+    applyNameImplications(parent, name.name);
+    return;
   }
 
   let value: Expression;
 
   switch(propValue.type){
-  case "JSXExpressionContainer":
-    value = propValue.expression as Expression;
-  break;
+    case "JSXExpressionContainer":
+      value = propValue.expression as Expression;
+    break;
 
-  case "StringLiteral":
-    value = propValue;
-  break;
+    case "StringLiteral":
+      value = propValue;
+    break;
 
-  default:
-    throw Error.InvalidPropValue(propValue);
+    default:
+      throw Error.InvalidPropValue(propValue);
   }
 
   const prop = new Prop(name.name, value);
