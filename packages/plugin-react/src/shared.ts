61c269d39fb4e72d0ab700179afdc14915374d2e
Gabe Klein <gabe@termtm.com>
2021-02-07 21:05:07 -0700

    reimplement style-uid hash function

--- packages/plugin-react/src/shared.ts
+++ packages/plugin-react/src/shared.ts
@@ -1,5 +1,4 @@
 import { Expression } from '@babel/types';
-import { createHash } from 'crypto';
 import { StackFrame } from 'parse';
 import { Options, BabelFile } from 'types';
 
@@ -28,12 +27,24 @@ class SharedSingleton {
 
 export const Shared = new SharedSingleton();
 
-export function hash(data: string, length: number = 3){
-  return createHash("md5")		
-    .update(data)		
-    .digest('hex')		
-    .substring(0, length)
-}
+/**
+ * "cyrb53" hashing function, lifted from stack-overflow.
+ * Avoids needing to use "crypto" module.
+ * 
+ * https://stackoverflow.com/a/52171480/877165
+ * */
+export function hash(str = "", length = 3, seed = 0){
+  let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+  const out = 4294967296 * (2097151 & h2) + (h1>>>0);
+  return out.toString(32).substring(0, length);
+};
 
 export function ensureArray<T>(a: T | T[]){
   return Array.isArray(a) ? a : [a];
