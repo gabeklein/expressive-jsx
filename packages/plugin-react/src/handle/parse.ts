1563e337a83330dd372b7b7e76f2e8c11ecda7bb
Gabe Klein <gabe@termtm.com>
2021-02-21 13:47:16 -0500

    move ./parse to handle directory

--- packages/plugin-react/src/handle/parse.ts
+++ packages/plugin-react/src/handle/parse.ts
@@ -1,22 +1,31 @@
 import { ParseErrors } from 'errors';
 import { ensureArray } from 'shared';
-import type { NodePath } from '@babel/traverse';
-import type { Node } from '@babel/types';
+import type { NodePath as Path } from '@babel/traverse';
+import type { Node, Statement, Expression } from '@babel/types';
 
 const Oops = ParseErrors({
   NodeUnknown: "Unhandled node of type {1}",
   BadInputModifier: "Modifier input of type {1} not supported here!"
 })
 
-type PathFor<T extends Node['type']> = NodePath<Extract<Node, { type: T }>>;
-type Apply<K extends Node['type'], T> = (path: PathFor<K>, recipient: T) => void;
+type PathFor<T extends Node['type']> = Path<Extract<Node, { type: T }>>;
+type Apply<K extends Node['type'], T> = (this: T, path: PathFor<K>) => void;
 
 export type ParserFor<T> = {
   [K in Node['type']]?: Apply<K, T>;
 };
 
+export function parser<T>(using: ParserFor<T>){
+  return function(
+    this: any,
+    ast: Path<Statement> | Path<Expression>
+  ){
+    parse(ast, using, this)
+  }
+}
+
 export function parse<T>(
-  ast: NodePath<Node>,
+  ast: Path<Statement> | Path<Expression>,
   using: ParserFor<T>,
   target: T){
 
