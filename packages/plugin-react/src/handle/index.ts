ad636e8fc69f7aee82285de9069e6fae3d95b2d7
Gabe Klein <gabe@termtm.com>
2021-04-11 12:51:35 -0600

    merge iterate modules

--- packages/plugin-react/src/handle/index.ts
+++ packages/plugin-react/src/handle/index.ts
@@ -2,7 +2,6 @@ export * from './attributes';
 export * from './element'
 export * from './entry';
 export * from './iterate';
-export * from './iterateForX';
 export * from './object';
 export * from './switch';
 export * from './modifier';
