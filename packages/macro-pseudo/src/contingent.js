bfd9555582ab7c74af4de94f94136b7065a7a95b
Gabe Klein <gabe@termtm.com>
2021-09-01 18:45:44 -0600

    define css:inner: instruction

--- packages/macro-pseudo/src/contingent.js
+++ packages/macro-pseudo/src/contingent.js
@@ -21,4 +21,28 @@ function hasAlso(){
   }
 }
 
-export { hasAlso as has }
+function hasInner(){
+  let { body } = this;
+
+  if(!body)
+    return
+  
+  if(body.type == "BlockStatement")
+    body = body.get("body");
+  else
+    body = [body];
+
+  for(const item of body){
+    if(!item.type == "LabeledStatement")
+      throw new Error("css modifier blew up");
+
+    const className = " ." + pascalToDash(item.node.label.name);
+
+    this.setContingent(className, 5, item.get("body"))
+  }
+}
+
+export {
+  hasAlso as has,
+  hasInner as inner
+}
