ec5b1ae39058d1608245b1e2e9bd1121a700d74c
Gabe Klein <gabe@termtm.com>
2021-09-13 13:07:02 -0600

    move assert to own file

--- packages/plugin-jsx/src/syntax/index.ts
+++ packages/plugin-jsx/src/syntax/index.ts
@@ -8,37 +8,4 @@ export {
 export * from './assertion';
 export * from './construct';
 export * from './jsx';
-
-import type * as r from "@babel/traverse";
-import type * as t from "@babel/types";
-
-type Type = t.Node["type"];
-type Node<T extends Type> = t.Node & { type: T };
-type Path<T extends Type> = r.NodePath<t.Node & { type: T }>;
-
-export function assert(node: null | undefined, type: string, fields?: any): false;
-export function assert<T extends Type>(path: r.NodePath<any> | undefined | null, type: T, fields?: Partial<Node<T>>): path is Path<T>;
-export function assert<T extends Type>(node: t.Node | undefined | null, type: T, fields?: Partial<Node<T>>): node is Node<T>;
-export function assert<T extends Type, N extends Node<T>>(
-  node: t.Node | r.NodePath<t.Node> | null | undefined, type: T, fields?: Partial<N>): boolean {
-
-  if(!node || typeof node != "object")
-    return false;
-
-  if("node" in node)
-    node = node.node;
-
-  if(type !== node.type)
-      return false;
-      
-  if(fields)
-    for(const k in fields){
-      const expect = fields[k] as any;
-      const value = (node as any)[k];
-
-      if(expect !== value)
-        return false;
-    }
-    
-  return true;
-}
+export * from './nodes'
