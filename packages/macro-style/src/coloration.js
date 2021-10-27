421d43de23d89f483b36e13b567c4ee9a5416234
Gabe Klein <gabe@termtm.com>
2021-10-27 02:20:22 -0400

    support full background macro in bg

--- packages/macro-style/src/coloration.js
+++ packages/macro-style/src/coloration.js
@@ -1,34 +1,35 @@
 import { rgba, hsla } from './colors';
 
 export function bg(a){
-  let output;
-
   if(Array.isArray(a)){
+    let attrs = {};
+
     const [ head, ...tail ] = a;
 
     switch(head){
       case "rgb":
       case "rgba": {
         const { value } = rgba(...tail);
-        output = { backgroundColor: value };
+        attrs = { backgroundColor: value };
         break;
       }
 
       case "hsl":
       case "hsla": {
         const { value } = hsla(...tail);
-        output = { backgroundColor: value };
+        attrs = { backgroundColor: value };
         break;
       }
     }
-  }
-  else {
-    output = {
-      background: a
+
+    return {
+      style: attrs
     }
   }
 
   return {
-    style: output
+    attrs: {
+      background: this.arguments
+    }
   }
 }
