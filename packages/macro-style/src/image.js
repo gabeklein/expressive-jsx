1c4c02a847276d02e42c325486530d17e36594e5
Gabe Klein <gabe@termtm.com>
2021-10-27 02:01:52 -0400

    add require to background macro

--- packages/macro-style/src/image.js
+++ packages/macro-style/src/image.js
@@ -6,6 +6,50 @@ export function image(a){
   }
 }
 
+function requireExpression(value){
+  return {
+    type: "CallExpression",
+    callee: { type: "Identifier", name: "require" },
+    arguments: [
+      { type: "StringLiteral", value }
+    ]
+  }
+}
+
+export function background(value, size, position){
+  const style = {};
+
+  if(size)
+    style.backgroundSize = size;
+
+  if(position)
+    style.backgroundPosition = position;
+
+  if(/^\.\.?\//.test(value))
+    style.backgroundImage = {
+      type: "TemplateLiteral",
+      expressions: [
+        requireExpression(value)
+      ],
+      quasis: [
+        {
+          type: "TemplateElement",
+          value: { raw: "url(", cooked: "url(" },
+          tail: false
+        },
+        {
+          type: "TemplateElement",
+          value: { raw: ")", cooked: ")" },
+          tail: true
+        }
+      ]
+    }
+  else
+    style.background = value;
+
+  return { style };
+}
+
 export function backgroundImage(a){
   if(typeof a == "object" && !a.named)
     return { style: {
