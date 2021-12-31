7953f5773df201365ce7ec2f76c196993d3e5869
Gabe Klein <gabe@termtm.com>
2021-12-31 13:31:10 -0500

    fallback if path module is not available

--- packages/plugin-jsx/src/parse/filename.ts
+++ packages/plugin-jsx/src/parse/filename.ts
@@ -1,13 +1,18 @@
-import { basename, dirname, sep as separator } from 'path';
-
 import type { Hub } from '@babel/traverse';
 
 export function getLocalFilename(hub: Hub){
-  const url = (hub as any).file.opts.filename as string;
-  const [ base ] = basename(url).split(".");
+  try {
+    const { basename, dirname, sep: separator } = require('path');
 
-  if(base !== "index")
-    return base;
-
-  return dirname(url).split(separator).pop()!;
+    const url = (hub as any).file.opts.filename as string;
+    const [ base ] = basename(url).split(".");
+  
+    if(base !== "index")
+      return base;
+  
+    return dirname(url).split(separator).pop()!;
+  }
+  catch(err){
+    return "File"
+  }
 }
