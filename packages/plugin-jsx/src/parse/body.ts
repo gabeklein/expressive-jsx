737424ac3408f7cdda344a3efd3c891fadb8061b
Gabe Klein <gabe@termtm.com>
2022-02-22 00:41:21 -0500

    cleanup define parsing

--- packages/plugin-jsx/src/parse/body.ts
+++ packages/plugin-jsx/src/parse/body.ts
@@ -27,66 +27,68 @@ export function parse(
     : [ast];
   
   for(const item of content)
-    parseContent(target, item);
+    switch(item.type){
+      case "LabeledStatement": 
+        handleDefine(target, item);
+      break
+
+      case "IfStatement":
+        handleIfStatement(target, item);
+      break;
+
+      case "ExpressionStatement":
+        handleExpression(target, item);
+      break;
+
+      case "ForInStatement":
+      case "ForOfStatement":
+      case "ForStatement":
+        new ComponentFor(item, target);
+      break;
+
+      default:
+        target.statements.push(item.node);
+    }
 }
 
-export function parseContent(
-  target: Define, path: t.Path<any>){
+function handleExpression(
+  target: Define, path: t.Path<t.IfStatement>){
 
-  if(s.assert(path, "LabeledStatement")){
-    handleDefine(target, path);
+  const e = path.get("expression") as t.Path<t.Node>;
 
-    return;
-  }
-  
-  if(s.assert(path, "IfStatement")){
-    const test = path.node.test;
+  if(s.assert(e, "JSXElement"))
+    addElementFromJSX(e, target);
 
-    if(s.assert(test, "StringLiteral")){
-      const body = path.get("consequent") as any;
-      const mod = new DefineVariant(target, [ test.value ], 5);
+  else if(s.assert(e, "AssignmentExpression", { operator: "=" })){
+    const { left, right } = e.node;
 
-      mod.onlyWithin = target.onlyWithin;
+    if(!s.assert(left, "Identifier"))
+      throw Oops.PropNotIdentifier(left)
 
-      parse(mod, body);
-      target.use(mod);
-
-      return
-    }
-
-    const item = new ComponentIf();
-    item.setup(target.context, path);
-    target.adopt(item);
-
-    return;
-  }
-
-  if(s.assert(path, ["ForInStatement", "ForOfStatement", "ForStatement"])){
-    new ComponentFor(path, target);
-    return;
-  }
-
-  if(s.assert(path, "ExpressionStatement")){
-    const e = path.get("expression") as t.Path<t.Node>;
-
-    if(s.assert(e, "JSXElement")){
-      addElementFromJSX(e, target);
-      return;
-    }
-
-    if(s.assert(e, "AssignmentExpression", { operator: "=" })){
-      const { left, right } = e.node;
-  
-      if(left.type !== "Identifier")
-        throw Oops.PropNotIdentifier(left)
-  
-      const prop = new Prop(left.name, right);
-        
-      target.add(prop);
+    const prop = new Prop(left.name, right);
       
-      return;
-    }
+    target.add(prop);
+  }
+}
+
+function handleIfStatement(
+  target: Define, path: t.Path<t.IfStatement>){
+
+  const test = path.node.test;
+
+  if(s.assert(test, "StringLiteral")){
+    const body = path.get("consequent") as any;
+    const mod = new DefineVariant(target, [ test.value ], 5);
+
+    mod.onlyWithin = target.onlyWithin;
+
+    parse(mod, body);
+    target.use(mod);
+    return;
   }
 
-  target.statements.push(path.node);
+  const item = new ComponentIf();
+
+  item.setup(target.context, path);
+  target.adopt(item);    
 }
