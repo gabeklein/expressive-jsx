06cc9c11d6db71bd5e3a660cf45ec877e8d4b9cb
Gabe Klein <gabe@termtm.com>
2022-03-16 21:29:01 -0400

    remove unused

--- packages/plugin-jsx/src/parse/parent.ts
+++
@@ -1,9 +0,0 @@
-import { getContext } from 'context';
-import type * as t from 'syntax/types';
-
-export function getTarget(path: t.Path<any>){
-  const context = getContext(path, true);
-  const target = context.ambient;
-
-  return target;
-}
