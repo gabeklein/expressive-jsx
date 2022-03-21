bde12b1ff8acad459a13c3a42dd82881242d714e
Gabe Klein <gabe@termtm.com>
2022-03-21 01:32:24 -0400

    use function name in entry

--- packages/plugin-jsx/src/index.ts
+++ packages/plugin-jsx/src/index.ts
@@ -3,6 +3,7 @@ import { OUTPUT_NODE } from 'generate/jsx';
 import { styleDeclaration } from 'generate/styles';
 import { Define, ElementInline } from 'handle/definition';
 import { parse } from 'parse/body';
+import { containerName } from 'parse/entry';
 import { parseJSX } from 'parse/jsx';
 import * as $ from 'syntax';
 
@@ -60,7 +61,8 @@ const JSXElement: Visitor<t.JSXElement> = {
       parent = parent.parentPath;
 
     const block = parent as unknown as t.Path<t.BlockStatement>;
-    const ambient = new Define(context);
+    const name = containerName(block as any);
+    const ambient = new Define(context, name);
 
     parse(ambient, block);
 
