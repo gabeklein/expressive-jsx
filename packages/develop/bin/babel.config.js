300c23ce91deaedecde11bc644b31e67e5563215
Gabe Klein <gabe@termtm.com>
2023-06-16 15:26:14 -0400

    consolidate develop babel config

--- packages/develop/bin/babel.config.js
+++ packages/develop/bin/babel.config.js
@@ -1,34 +1,13 @@
-const env = require("@babel/preset-env");
-
-const BabelPluginReact = require("@expressive/babel-plugin-jsx");
-const webstyleMacros = require("@expressive/macro-css");
-const gradientMacros = require("@expressive/macro-gradient");
-const classProperties = require("@babel/plugin-syntax-class-properties");
-
-const { 
-  TARGET = "jsx"
-} = process.env;
-
 module.exports = {
   comments: false,
-  presets: [
-    [env, {
-      "targets": {
-        node: "current"
-      },
-      modules: false
-    }]
-  ],
   plugins: [
-    classProperties,
-    [BabelPluginReact, {
+    ["@expressive/babel-plugin-jsx", {
       hot: false, 
-      output: TARGET,
+      output: process.env.TARGET || "jsx",
       printStyle: "pretty",
-      // styleMode: "inline",
       modifiers: [
-        webstyleMacros,
-        gradientMacros
+        require("@expressive/macro-css"),
+        require("@expressive/macro-gradient")
       ]
     }]
   ]
