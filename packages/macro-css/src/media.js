ea652dab54a0d4501b7a98d240f5b15194c772e5
Gabe Klein <gabe@termtm.com>
2023-06-18 15:15:06 -0400

    fix syntax for css-macro media module

--- packages/macro-css/src/media.js
+++ packages/macro-css/src/media.js
@@ -22,30 +22,35 @@ const KEYWORD = {
   portrait: "(orientation: portrait)"
 }
 
-function Transform(e){
+function handleQuery(e){
   if(typeof e == "string" && KEYWORD[e])
     return KEYWORD[e];
-  else if(e.type == "binary"){
-    let { operator, right, left } = e;
-    if(operator[1] == "=="){
-      right += MIN_MAX[operator];
-      operator = operator[0]
-    }
 
-    let op = MIN_MAX[operator];
-    let key = SIZE[left];
+  if(e.type != "binary")
+    return;
 
-    if(key && typeof right == "number") right += "px";
-    else key = SPECIAL[left]
+  let { operator, right, left } = e;
 
-    if(key && op !== undefined){
-      return `(${op}${key}: ${right})`
-    }
+  if(operator[1] == "=="){
+    right += MIN_MAX[operator];
+    operator = operator[0]
   }
+
+  const op = MIN_MAX[operator];
+  let key = SIZE[left];
+
+  if(key && typeof right == "number")
+    right += "px";
+  else
+    key = SPECIAL[left]
+
+  if(key && op !== undefined)
+    return `(${op}${key}: ${right})`
 }
 
 export function screen(){
   let { body } = this;
+
   body = body.type == "BlockStatement"
     ? body.get("body")
     : body.type == "IfStatement"
@@ -53,15 +58,17 @@ export function screen(){
     : [];
 
   for(const node of body)
-  if(node.type == "IfStatement"){
-    let query = this.parse(node.get("test"));
-      query = query.map(Transform).join(" and ");
+    if(node.type == "IfStatement"){
+      const query = this
+        .parse(node.get("test"))
+        .map(handleQuery)
+        .join(" and ");
 
-    this.declareMediaQuery(
-      'only screen' + (query ? ' and ' + query : ''),
-      node.get("consequent")
-    );
-  }
+      this.declareMediaQuery(
+        'only screen' + (query ? ' and ' + query : ''),
+        node.get("consequent")
+      );
+    }
 }
 
 export function mobile(){
