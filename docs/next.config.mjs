4c0f7fb226dea5b2187da23051c78389d60202f6
Gabe Klein <gabe@termtm.com>
2023-07-02 19:16:10 -0400

    initialize docs

---
+++ docs/next.config.mjs
@@ -0,0 +1,16 @@
+import Nextra from 'nextra';
+import ExpressivePlugin from '@expressive/webpack-plugin';
+
+const nextra = Nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+});
+
+export default nextra({
+  webpack: (config) => {
+    config.plugins.push(
+      new ExpressivePlugin()
+    );
+    return config;
+  }
+});
