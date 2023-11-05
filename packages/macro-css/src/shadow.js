11b00517bbce59154455d42aab254327addc68f6
Gabe Klein <gabe@termtm.com>
2023-11-05 17:26:11 -0500

    delete macro-css

--- packages/macro-css/src/shadow.js
+++
@@ -1,9 +0,0 @@
-export function shadow(color, radius = 10, x = 2, y = x){
-  const boxShadow = color == "intial" || color == "none"
-    ? color
-    : `${x}px ${y}px ${radius}px ${color}`;
-
-  return {
-    boxShadow
-  }
-}
