83f88dcf0a9b251fab57ca5ee048652965a24720
Gabe Klein <gabe@termtm.com>
2024-02-10 12:29:27 -0500

    move classNames helper

--- packages/babel-plugin-jsx-v2/src/syntax/className.ts
+++ packages/babel-plugin-jsx-v2/src/syntax/className.ts
@@ -1,5 +1,5 @@
 import * as t from '../types';
-import { uniqueIdentifier } from './unique';
+import { importClassNamesHelper } from './polyfill';
 
 export function setClassNames(
   path: t.NodePath<t.JSXElement>,
@@ -44,37 +44,6 @@ export function setClassNames(
         classNamePropValue
       )
     );
-}
-
-const POLYFILL = require.resolve("../../polyfill");
-
-export function importClassNamesHelper(path: t.NodePath){
-  const program = path.find(x => x.isProgram()) as t.NodePath<t.Program>;
-  const body = program.get("body");
-
-  for(const statement of body){
-    if(!statement.isImportDeclaration() || statement.node.source.value !== POLYFILL)
-      continue;
-  
-    const specifiers = statement.get("specifiers");
-
-    for(const spec of specifiers){
-      if(!spec.isImportSpecifier())
-        continue;
-
-      if(t.isIdentifier(spec.node.imported, { name: "classNames"}))
-        return spec.node.local;
-    }
-  }
-
-  const id = uniqueIdentifier(path.scope, "classNames");
-  const importStatement = t.importDeclaration(
-    [t.importSpecifier(id, t.identifier("classNames"))],
-    t.stringLiteral(POLYFILL)
-  );
-
-  program.node.body.unshift(importStatement);
-  return id;
 }
 
 function optimizeClassNames(expressions: t.Expression[]){
---
+++ packages/babel-plugin-jsx-v2/src/syntax/polyfill.ts
@@ -0,0 +1,33 @@
+import * as t from '../types';
+import { uniqueIdentifier } from './unique';
+
+const POLYFILL =  require.resolve("../../polyfill");
+
+export function importClassNamesHelper(path: t.NodePath) {
+  const program = path.find(x => x.isProgram()) as t.NodePath<t.Program>;
+  const body = program.get("body");
+
+  for (const statement of body) {
+    if (!statement.isImportDeclaration() || statement.node.source.value !== POLYFILL)
+      continue;
+
+    const specifiers = statement.get("specifiers");
+
+    for (const spec of specifiers) {
+      if (!spec.isImportSpecifier())
+        continue;
+
+      if (t.isIdentifier(spec.node.imported, { name: "classNames" }))
+        return spec.node.local;
+    }
+  }
+
+  const id = uniqueIdentifier(path.scope, "classNames");
+  const importStatement = t.importDeclaration(
+    [t.importSpecifier(id, t.identifier("classNames"))],
+    t.stringLiteral(POLYFILL)
+  );
+
+  program.node.body.unshift(importStatement);
+  return id;
+}
