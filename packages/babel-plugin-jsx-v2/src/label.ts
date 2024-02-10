b9523dfe854653aa1a5e573cc11b77e701161ef9
Gabe Klein <gabe@termtm.com>
2024-02-10 14:06:00 -0500

    improved label handling

--- packages/babel-plugin-jsx-v2/src/label.ts
+++ packages/babel-plugin-jsx-v2/src/label.ts
@@ -32,59 +32,68 @@ export function handleLabel(
   if(!(parent instanceof DefineContext))
     throw new Error("Invalid modifier");
 
-  const args = new Parser(body.node).arguments;
-  const queue: ModifierItem = [{ name, args, body }];
-
   try {
-    while(queue.length){
-      const { name, args } = queue.pop()!;
-      const apply = (args: any[]) => {
-
-        parent.assign(name, ...args);
-      }
-
-      if(!(parent instanceof DefineContext))
-        throw new Error("Invalid modifier");
-
-      const macro = parent.macros[name];
-
-      if(!macro){
-        apply(args);
-        continue;
-      }
-
-      const output = macro.apply(parent as DefineContext, args);
-
-      if(!output)
-        continue;
-
-      if(Array.isArray(output)){
-        apply(output);
-        continue;
-      }
-      
-      if(typeof output != "object")
-        throw new Error("Invalid modifier output.");
-
-      for(const key in output){
-        let args = output[key];
-
-        if(args === undefined)
-          continue;
-
-        if(!Array.isArray(args))
-          args = [args];
-
-        if(key === name)
-          apply(args);
-        else
-          queue.push({ name: key, args });
-      }
-    }
+    applyMacros(parent, name, body);
   }
   catch(err: unknown){
     throw parseError(body, err, name);
   }
+}
+
+function applyMacros(
+  parent: DefineContext,
+  name: string,
+  body: t.NodePath<t.ExpressionStatement>){
+
+  const args = new Parser(body.node).arguments;
+  const queue: ModifierItem = [{ name, args, body }];
+
+  while(queue.length){
+    const { name, args } = queue.pop()!;
+    const apply = (args: any[]) => {
+
+      parent.assign(name, ...args);
+    }
+
+    if(!(parent instanceof DefineContext))
+      throw new Error("Invalid modifier");
+
+    const macro = parent.macros[name];
+
+    if(!macro){
+      apply(args);
+      continue;
+    }
+
+    const output = macro.apply(parent as DefineContext, args);
+
+    if(!output)
+      continue;
+
+    if(Array.isArray(output)){
+      apply(output);
+      continue;
+    }
+    
+    if(typeof output != "object")
+      throw new Error("Invalid modifier output.");
+
+    for(const key in output){
+      let args = output[key];
+
+      if(args === undefined)
+        continue;
+
+      if(!Array.isArray(args))
+        args = [args];
+
+      if(key === name)
+        apply(args);
+      else
+        queue.push({ name: key, args });
+    }
+  }
+
 }
 
 function parseError(path: t.NodePath, err: unknown, modiferName: string){
@@ -92,9 +101,8 @@ function parseError(path: t.NodePath, err: unknown, modiferName: string){
     return path.hub.buildError(path.node, `Modifier "${modiferName}" failed: ${err}`);
 
   const stack = err.stack!.split("\n    at ");
-  const error = path.hub.buildError(path.node, `Modifier "${modiferName}" failed: ${
-    err instanceof Error ? err.message : err
-  }`);
+  const message = err instanceof Error ? err.message : err;
+  const error = path.hub.buildError(path.node, `Modifier "${modiferName}" failed: ${message}`);
 
   error.stack = stack.slice(0, stack.findIndex(line => /^parse/.test(line)) + 1).join("\n    at ");
   
