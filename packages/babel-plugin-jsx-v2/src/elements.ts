d9ff8ce863edbf947849e867f33cebe3389ec4d5
Gabe Klein <gabe@termtm.com>
2024-03-02 16:33:48 -0500

    simplify ElementContext

--- packages/babel-plugin-jsx-v2/src/elements.ts
+++ packages/babel-plugin-jsx-v2/src/elements.ts
@@ -7,23 +7,34 @@ import * as t from './types';
 export function handleElement(
   path: t.NodePath<t.JSXElement>){
   
-  const element = new ElementContext(path);
+  const parent = getContext(path);
   const opening = path.get("openingElement");
-  let tag = opening.node.name;
+  const tagName = opening.get("name");
+  const element = new ElementContext(tagName.toString(), parent);
+
+  function use(name: string){
+    const apply = element.get(name);
+
+    apply.forEach(x => {
+      x.usedBy.add(element);
+      element.using.add(x);
+    });
+
+    return apply.length > 0;
+  }
+
+  let tag = tagName.node;
 
   while(t.isJSXMemberExpression(tag)){
-    element.use(tag.property.name);
+    use(tag.property.name);
     tag = tag.object;
   }
 
   if(t.isJSXIdentifier(tag))
-    element.use(tag.name);
-
-  if(!hasProperTagName(path))
-    setTagName(path, "div");
+    use(tag.name);
 
   if(t.isJSXIdentifier(tag))
-    element.use(tag.name);
+    use(tag.name);
 
   opening.get("attributes").forEach((attr) => {
     if(!attr.isJSXAttribute() || attr.node.value)
@@ -34,76 +45,53 @@ export function handleElement(
     if(typeof name !== "string")
       name = name.name;
     
-    if(element.use(name))
+    if(use(name))
       attr.remove();
   });
 
-  element.commit();
+  const names: t.Expression[] = []
+  let forward = false;
+  
+  for(const context of element.using){
+    let { className } = context;
+
+    if(context instanceof FunctionContext)
+      forward = true;
+
+    if(typeof className == 'string')
+      className = t.stringLiteral(className);
+
+    if(className)
+      names.push(className);
+  }
+
+  if(forward){
+    const props = forwardProps(path);
+
+    if(names.length)
+      names.unshift(extractClassName(props, path.scope))
+  }
+
+  if(names.length > 0)
+    setClassNames(path, names);
+
+  if(!hasProperTagName(path))
+    setTagName(path, "div");
+
+  element.assignTo(path);
 }
 
 export class ElementContext extends Context {
-  parent: Context;
-  
   using = new Set<DefineContext>();
   props: Record<string, t.Expression> = {};
   styles: Record<string, t.Expression | string> = {};
 
-  constructor(public path: t.NodePath<t.JSXElement>){
-    const tagName = path.get("openingElement.name").toString();
-    const parent = getContext(path);
-
-    super(tagName);
-
-    this.parent = parent;
-    this.assignTo(path);
-  }
-
   get(name: string){
     const mods = new Set<DefineContext>();
 
-    for(const ctx of [this.parent, ...this.using])
+    for(const ctx of [this.parent!, ...this.using])
       ctx.get(name).forEach(x => mods.add(x));
 
     return Array.from(mods);
-  }
-
-  use(name: string){
-    const apply = this.get(name);
-
-    apply.forEach(x => {
-      x.usedBy.add(this);
-      this.using.add(x);
-    });
-
-    return apply.length > 0;
-  }
-
-  commit(){
-    const { using, path } = this;
-    const names: t.Expression[] = []
-    let forward = false;
-    
-    for(const context of using){
-      let { className } = context;
-
-      if(context instanceof FunctionContext)
-        forward = true;
-
-      if(typeof className == 'string')
-        className = t.stringLiteral(className);
-
-      if(className)
-        names.push(className);
-    }
-
-    if(forward){
-      const props = forwardProps(path);
-
-      if(names.length)
-        names.unshift(extractClassName(props, path.scope))
-    }
-
-    if(names.length > 0)
-      setClassNames(path, names);
   }
 }
