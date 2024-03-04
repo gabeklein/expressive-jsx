ae6a266895c4dc87d28e2182b52c58cc7b638b68
Gabe Klein <gabe@termtm.com>
2024-03-04 16:08:57 -0500

    adjust types

--- packages/babel-preset-react/src/index.ts
+++ packages/babel-preset-react/src/index.ts
@@ -1,10 +1,11 @@
+import { PluginItem } from '@babel/core';
 import JSX, { Options } from '@expressive/babel-plugin-jsx';
 
 import { addStyle, CSS } from './css';
 import * as Macros from './macros';
 import * as Pseudo from './pseudo';
 
-const Preset = (_compiler: any, options: Options = {}) => {
+const Preset: PluginItem = (_compiler: any, options: Options = {}) => {
   let { macros = [], output = "js", ...opts } = options;
 
   return {
