c1b3160ed6a3d21259f638dd5807196e5ca108a0
Gabe Klein <gabe@termtm.com>
2024-03-07 11:50:25 -0500

    collapse fixImplictReturn

--- packages/babel-preset-web/src/plugin.ts
+++ packages/babel-preset-web/src/plugin.ts
@@ -107,7 +107,19 @@ const JSXElement: Visitor<t.JSXElement> = {
     if(apply)
       apply(element);
 
-    fixImplicitReturn(path);
+    const statement = path.parentPath;
+    const block = statement.parentPath!;
+  
+    if (!block.isBlockStatement())
+      return;
+  
+    const within = block.parentPath as t.NodePath;
+    const inserted = block.node.body.length === 1 &&
+      within.isArrowFunctionExpression()
+      ? block.replaceWith(t.parenthesizedExpression(path.node))
+      : statement.replaceWith(t.returns(path.node));
+  
+    inserted[0].skip();
   }
 }
 
--- packages/babel-preset-web/src/syntax/element.ts
+++ packages/babel-preset-web/src/syntax/element.ts
@@ -73,19 +73,3 @@ export function extractProperty(
 
   return value;
 }
-
-export function fixImplicitReturn(path: t.NodePath<t.Expression>) {
-  const statement = path.parentPath;
-  const block = statement.parentPath!;
-
-  if (!block.isBlockStatement())
-    return;
-
-  const within = block.parentPath as t.NodePath;
-  const inserted = block.node.body.length === 1 &&
-    within.isArrowFunctionExpression()
-    ? block.replaceWith(t.parenthesizedExpression(path.node))
-    : statement.replaceWith(t.returns(path.node));
-
-  inserted[0].skip();
-}
