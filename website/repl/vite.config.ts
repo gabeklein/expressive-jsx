1d9486037707318171f31115c9c0770f0c1d46c8
Gabe Klein <gabe@termtm.com>
2025-01-30 01:08:43 -0500

    use tsx for home module

--- website/repl/vite.config.ts
+++ website/repl/vite.config.ts
@@ -1,4 +1,4 @@
-import jsx from '@expressive/vite-plugin/src';
+import jsx from '@expressive/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import paths from 'vite-tsconfig-paths';
