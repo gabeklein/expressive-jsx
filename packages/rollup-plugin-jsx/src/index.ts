a26e7e45a8940ca3db3cf90a3e9e1e8564701ea5
Gabe Klein <gabe@termtm.com>
2025-05-12 14:17:13 -0400

    refactor: restructure expressiveJSX plugin and extract transform function

--- packages/rollup-plugin-jsx/src/index.ts
+++ packages/rollup-plugin-jsx/src/index.ts
@@ -1,28 +1,21 @@
 // import generate from '@babel/generator';
 // import traverse from '@babel/traverse';
-import { transformAsync } from '@babel/core';
 import { createFilter } from '@rollup/pluginutils';
-import BabelPreset from '@expressive/babel-preset';
 import path from 'path';
+import { Plugin } from 'rollup';
 
-import type { Plugin } from 'rollup';
+import { transform } from './transform';
 
 type Options = {
   include?: string | string[];
   exclude?: string | string[];
 };
 
-interface TransformResult {
-  code: string;
-  map: any;
-  css: string;
-}
-
-function cssLabeledPlugin(options: Options = {}): Plugin{
+function expressiveJSX(options: Options = {}): Plugin {
   const filter = createFilter(options.include || ['**/*.jsx']);
 
   return {
-    name: 'rollup-plugin-css-labeled',
+    name: 'rollup-plugin-expressive-jsx',
 
     async transform(code, id) {
       if (!filter(id)) return null;
@@ -37,22 +30,6 @@ function cssLabeledPlugin(options: Options = {}): Plugin{
           cssFilename: `./${path.basename(id).replace(/\.[jt]sx?$/, '')}.css`,
         },
       }
-
-      // // Add import statement to the AST
-      // const cssFilename = `./${path.basename(id).replace(/\.[jt]sx?$/, '')}.css`;
-      // // const importDecl = t.importDeclaration([], t.stringLiteral(cssFilename));
-      // // ast.program.body.unshift(importDecl);
-
-      // const output = generate(result.ast!, {}, code);
-
-      // return {
-      //   code: result.code,
-      //   map: result.map,
-      //   meta: {
-      //     css: cssChunks.join('\n'),
-      //     cssFilename,
-      //   },
-      // };
     },
 
     generateBundle(_, bundle) {
@@ -71,48 +48,4 @@ function cssLabeledPlugin(options: Options = {}): Plugin{
   };
 }
 
-async function transform(
-  id: string,
-  input: string,
-  presetOptions: BabelPreset.Options = {}
-): Promise<TransformResult> {
-  const result = await transformAsync(input, {
-    root: process.cwd(),
-    filename: id,
-    sourceFileName: id.split("?")[0],
-    sourceMaps: true,
-    parserOpts: {
-      sourceType: "module",
-      allowAwaitOutsideFunction: true
-    },
-    generatorOpts: {
-      decoratorsBeforeExport: true
-    },
-    presets: [
-      [BabelPreset, presetOptions]
-    ]
-  });
-
-  if (!result)
-    throw new Error("No result");
-
-  let {
-    code,
-    map,
-    metadata
-  } = result;
-
-  let css = (metadata as any)?.css || "";
-
-  if (!code)
-    throw new Error("No code");
-
-  return <TransformResult> {
-    code,
-    css,
-    map
-  };
-}
-
-
-export = cssLabeledPlugin;
+export default expressiveJSX;
